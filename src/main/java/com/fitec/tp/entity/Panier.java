9e9b63aed1fb8d670d589d26611c61c87e9f2d9a
webnduck <maria.hardy@numericable.fr>
2017-06-09 17:36:10 +0200

    Ajout entité Panier

---
+++ src/main/java/com/fitec/tp/entity/Panier.java
@@ -0,0 +1,28 @@
+package com.fitec.tp.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+
+public class Panier implements Serializable, IEntity {
+
+	List<LigneCommande> listeLignesCommandes;
+
+	User id_user;
+
+	public List<LigneCommande> getListeLignesCommandes() {
+		return listeLignesCommandes;
+	}
+
+	public void setListeLignesCommandes(List<LigneCommande> listeLignesCommandes) {
+		this.listeLignesCommandes = listeLignesCommandes;
+	}
+
+	public User getId_user() {
+		return id_user;
+	}
+
+	public void setId_user(User id_user) {
+		this.id_user = id_user;
+	}
+}
